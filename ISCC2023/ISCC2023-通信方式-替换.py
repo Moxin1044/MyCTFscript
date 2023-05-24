d7724de855ccb05ef0b6088c1f702275af99ff5a
刘骐宁 <1044631097@qq.com>
2023-05-25 00:11:30 +0800

    Initial commit

---
+++ ISCC2023/ISCC2023-通信方式-替换.py
@@ -0,0 +1,33 @@
+original_text = "内容"
+replacement_dict = {
+    "可艾": "K",
+    "斯一": "C",
+    "艾偶": "L",
+    "艾福": "F",
+    "达不溜": "W",
+    "滋一": "Z",
+    "艾克斯": "X",
+    "科一由": "Q",
+    "艾木": "M",
+    "之": "Z",
+    "歪": "Y",
+    "第": "D",
+    "鸡": "J",
+    "偶": "O",
+    "艾": "I",
+    "斯": "S",
+    "由": "U",
+    "皮": "P",
+    "危": "V",
+    "替": "T",
+    "不": "B",
+    "溜": "L",
+    "恩": "N",
+    "吃": "E",
+    "括弧": "{"
+}
+
+for key, value in replacement_dict.items():
+    original_text = original_text.replace(key, value)
+
+print(original_text)  # 翻转一下最后一个字符不会死
