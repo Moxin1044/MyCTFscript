22fded691d19b9fba0fd5b613cab718c632f5c7a
Moxin <1044641097@qq.com>
2023-07-14 23:06:39 +0800

    feat: 增加两个通信方式修改出题脚本

---
+++ 出题/修改脚本/ISCC2023-通信方式-替换（修改）.py
@@ -0,0 +1,37 @@
+original_text = "KACLSFWZXQMJH"
+replacement_dict = {
+    "可艾": "K",
+    "艾一": "A",
+    "斯一": "C",
+    "艾偶": "L",
+    "艾斯": "S",
+    "艾福": "F",
+    "达不溜": "W",
+    "滋一": "Z",
+    "艾克斯": "X",
+    "科一由": "Q",
+    "艾木": "M",
+    "之艾": "J",
+    "艾吃": "H",
+    "啊": "R",
+    "比": "B",
+    "歪": "Y",
+    "第": "D",
+    "易": "E",
+    "鸡": "G",
+    "偶": "O",
+    "艾": "I",
+    "由": "U",
+    "皮": "P",
+    "危": "V",
+    "替": "T",
+    "不": "B",
+    "溜": "L",
+    "恩": "N",
+    "括弧": "{"
+}
+
+for key, value in replacement_dict.items():
+    original_text = original_text.replace(value, key)
+
+print(original_text)  # 翻转一下最后一个字符不会死
