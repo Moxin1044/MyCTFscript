a831bd56f1ba56bfcc91b19cdfb1cda88631b072
Moxin <1044641097@qq.com>
2023-08-05 00:16:17 +0800

    feat: 增加用户信息接口

--- Platform_support/QSNCTF/get_login_user_info.py
+++ Platform_support/QSNCTF/get_login_user_info.py
@@ -24,8 +24,8 @@ def get_qsnctf_session():
         soup = BeautifulSoup(response.text, 'html.parser')
         nonce_value = soup.find('input', {'id': 'nonce'})['value']
         login_data = {
-            'name': 'DemoUser1',
-            'password': 'DemoUser1',
+            'name': '用户名',
+            'password': '密码',
             '_submit': '登录',
             'nonce': nonce_value
         }
@@ -51,28 +51,17 @@ def get_qsnctf_session():
 
 
 def get_login_user_info(cookies):
-    import requests
-
-    url = "https://www.qsnctf.com:443/api/v1/users/me"
-    cookies = {
-        "session": cookies,
-    }
-    headers = {
-        "Sec-Ch-Ua": "\"Chromium\";v=\"109\", \"Not_A Brand\";v=\"99\"",
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Csrf-Token": "0d5cd47080cbea986590b3ee8cc0bde17fc9105e134d7dc86a83b66f27e82298",
-        "Sec-Ch-Ua-Mobile": "?0",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.75 Safari/537.36",
-        "Sec-Ch-Ua-Platform": "\"Windows\"",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Dest": "empty",
-        "Referer": "https://www.qsnctf.com/challenges",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9"
-    }
-    response = requests.get(url, headers=headers, cookies=cookies)
+    url = "https://www.qsnctf.com/api/v1/users/me"
+    cookies = cookies
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0",
+                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                     "Accept-Encoding": "gzip, deflate",
+                     "Referer": "https://www.qsnctf.com/login?next=%2Fscoreboard%2Fuser%3F",
+                     "Upgrade-Insecure-Requests": "1", "Sec-Fetch-Dest": "document", "Sec-Fetch-Mode": "navigate",
+                     "Sec-Fetch-Site": "same-origin", "Sec-Fetch-User": "?1", "Te": "trailers", "Connection": "close"}
+    response = requests.get(url, headers=burp0_headers, cookies=cookies)
+    return response.text
 
 
 print(get_login_user_info(get_qsnctf_session()))
