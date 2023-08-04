659636f45d4a9402b1a7994020ac770f35346fa9
Moxin <1044641097@qq.com>
2023-08-05 00:00:07 +0800

    fix: 修复nonce导致无法正常登录的问题

--- Platform_support/QSNCTF/get_scoreboard.py
+++ Platform_support/QSNCTF/get_scoreboard.py
@@ -6,46 +6,49 @@ from bs4 import BeautifulSoup
 
 
 def get_qsnctf_session():
-    login_data = {
-        'name': 'user',
-        'password': 'pass',
-        '_submit': '登录',
-        'nonce': '9dd6a46f23c0c07e7ec8464cc29bd7cb6f4b83c91a8d56f3f384e8cc9e2bd4f7'
-    }
-
-    headers = {
-        "Host": "www.qsnctf.com",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Encoding": "identity",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Cache-Control": "max-age=0",
-        "Content-Length": "120",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Cookie": "session=2baf1b47-8572-4ee8-9c97-02807dd84728.rmy-PyEq15UtODPuuZAf4Kh2Emg; Hm_lvt_10309f8528ef7f3bdd779aa12ad6dc7e=1687517039; _ga=GA1.1.738331937.1687517039; Hm_lpvt_10309f8528ef7f3bdd779aa12ad6dc7e=1687517290; _ga_Y057G8H97X=GS1.1.1687517039.1.1.1687517650.0.0.0",
-        "Origin": "https://www.qsnctf.com",
-        "Referer": "https://www.qsnctf.com/login",
-        "Sec-Ch-Ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
-        "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Platform": "\"macOS\"",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-    }
-
-    # 发送登录请求
-    response = requests.post('https://www.qsnctf.com/login', data=login_data, headers=headers, allow_redirects=False)
-
-    # 检查登录是否成功
-    if response.status_code == 200 and '<span>用户名或密码错误</span>' in response.text:
-        print("登录失败，用户名或密码错误。")
-        return 'error'
-    elif response.status_code == 302:
-        # 获取Cookie信息
+    burp0_headers = {"Cache-Control": "max-age=0", "Sec-Ch-Ua": "\"Chromium\";v=\"109\", \"Not_A Brand\";v=\"99\"",
+                     "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"Windows\"", "Upgrade-Insecure-Requests": "1",
+                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.75 Safari/537.36",
+                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                     "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1",
+                     "Sec-Fetch-Dest": "document", "Referer": "https://www.qsnctf.com/",
+                     "Accept-Encoding": "gzip, deflate", "Accept-Language": "zh-CN,zh;q=0.9"}
+    # 先访问一次平台，获取Cookie：
+    response = requests.get("https://www.qsnctf.com/login", headers=burp0_headers)
+    if response.status_code == 200:
         cookies = response.cookies
-        return cookies
+        session = ""
+        for name, value in cookies.items():
+            if name == "session":
+                session = value
+        soup = BeautifulSoup(response.text, 'html.parser')
+        nonce_value = soup.find('input', {'id': 'nonce'})['value']
+        login_data = {
+            'name': '用户名',
+            'password': '密码',
+            '_submit': '登录',
+            'nonce': nonce_value
+        }
+        burp0_cookies = {"session": session}
+        burp0_headers = {"Cache-Control": "max-age=0", "Sec-Ch-Ua": "\"Chromium\";v=\"109\", \"Not_A Brand\";v=\"99\"",
+                         "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"Windows\"",
+                         "Upgrade-Insecure-Requests": "1",
+                         "Origin": "https://www.qsnctf.com", "Content-Type": "application/x-www-form-urlencoded",
+                         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.75 Safari/537.36",
+                         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                         "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1",
+                         "Sec-Fetch-Dest": "document", "Referer": "https://www.qsnctf.com/login",
+                         "Accept-Encoding": "gzip, deflate", "Accept-Language": "zh-CN,zh;q=0.9"}
+        response = requests.post("https://www.qsnctf.com/login", headers=burp0_headers, cookies=burp0_cookies,
+                                 data=login_data, allow_redirects=False)
+        # 检查登录是否成功
+        if response.status_code == 200 and '<span>用户名或密码错误</span>' in response.text:
+            print("登录失败，用户名或密码错误。")
+        elif response.status_code == 302:
+            # 获取Cookie信息
+            cookies = response.cookies
+            session_value = cookies.get('session')
+        return session_value
 
 
 def reading(cookies):
