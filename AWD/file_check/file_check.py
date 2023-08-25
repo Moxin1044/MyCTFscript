3df1b51eb321630fabfb285fa1ee3b2f30f85275
Moxin <1044641097@qq.com>
2023-08-25 13:36:42 +0800

    feat( AWD ): ✨ 检测服务器文件变更

--- AWD/file_check/file_check.py
+++ AWD/file_check/file_check.py
@@ -3,11 +3,13 @@ import time
 import hashlib
 from stat import S_ISDIR
 
+
 def get_md5_checksum(client, file_path):
     stdin, stdout, stderr = client.exec_command(f"md5sum {file_path}")
     md5_output = stdout.read().decode()
     md5_checksum = md5_output.strip().split()[0]
     return md5_checksum
+
 
 def check_for_changes(client, sftp, path, files):
     changes = []
@@ -25,6 +27,7 @@ def check_for_changes(client, sftp, path, files):
                     changes.append(f"\033[91mChanged file: {file_path}\033[0m")
                     files[file_path] = md5_checksum
     return changes
+
 
 def monitor_directory_changes(hostname, username, password, directory_path):
     try:
