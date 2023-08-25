2d854552ba589c001f9506fdbbfb01554c142825
Moxin <1044641097@qq.com>
2023-08-25 13:30:08 +0800

    fix( ws ): 🐛 WebSocket不自动刷新

--- 好玩的/WebSocket_Read.py
+++ 好玩的/WebSocket_Read.py
@@ -1,15 +1,12 @@
 import asyncio
 import websockets
 
+
 async def read_data():
-    url = "ws://30.10.1.1/ws"
+    url = "ws://30.10.1.1/wsng"
     async with websockets.connect(url, ping_timeout=3600) as websocket:
         while True:
-            try:
-                data = await asyncio.wait_for(websocket.recv(), timeout=10)
-                print(f"Received data: {data}")
-            except asyncio.TimeoutError:
-                print("Timeout: No data received within 10 seconds")
-                break
+            data = await websocket.recv()
+            print(f"Received data: {data}")
 
 asyncio.get_event_loop().run_until_complete(read_data())
