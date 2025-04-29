899e07d2ea827bdce71fc9efca51d4b5a0dd60ef
Moxin1044 <1044631097@qq.com>
2025-04-29 21:36:49 +0800

    refactor(Crypto): 移除phi_n计算以简化代码
    移除pq=n.py中phi_n的计算，因为result仅使用n的值，简化了代码逻辑

---
+++ 出题/Crypto/phin.py
@@ -0,0 +1,5 @@
+p = 959571863
+q = 888450301
+phi_n = (p - 1) * (q - 1)
+flag = f"flag{{{phi_n}}}"
+print(flag)
--- 出题/Crypto/pq=n.py
+++ 出题/Crypto/pq=n.py
@@ -1,7 +1,5 @@
 p = 959571863
 q = 888450301
 n = p * q
-phi_n = (p - 1) * (q - 1)
-result = n + phi_n
-flag = f"flag{{{result}}}"
+flag = f"flag{{{n}}}"
 print(flag)
